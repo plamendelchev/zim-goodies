0b6e4081d3f00284aaa31d61b5f1d48014ac347c
Plamen Delchev <plamen.delchev@gmail.com>
2026-04-28 14:09:00 +0300

    oh-my-posh.zsh: Set OMP_THEME

--- oh-my-posh.zsh
+++ oh-my-posh.zsh
@@ -1,5 +1,6 @@
 # initialize ohp
 if [[ "$TERM_PROGRAM" != "Apple_Terminal" ]]; then
-  eval "$(oh-my-posh init zsh --config ~/.config/zsh/config.omp.toml)"
+  eval "$(oh-my-posh init zsh --config ~/.config/zsh/main.omp.toml)"
+  export OMP_THEME=main
 fi
 
